12e857ed3e666b5177ea6d20d3a8adbb1022304a
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2024-10-24 23:39:06 +0530

    Update TryCatch.js

--- middlewares/TryCatch.js
+++ middlewares/TryCatch.js
@@ -1,12 +1,12 @@
-const TryCatch = (handler) =>{
-    return async(req,res,next) =>{
-        try{
-            await handler(req,res,next);
-        } catch(error){
-          res.status(500).json({
-            message:error.message,
-        });
-    }
+const TryCatch = (handler) => {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res, next);
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
     };
 };
 
