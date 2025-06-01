956f933d3b5b47d0dee0ffa0c3cacfd52809a876
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2025-06-01 18:48:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,35 @@ import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 import cors from "cors";
 
-
 dotenv.config();
 
 const app = express();
 
-//using middlewares..
-
+// ✅ Use Middlewares
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
-const port = process.env.PORT;
+// ✅ Serve static files BEFORE routes
+app.use("/uploads", express.static("uploads"));
 
-app.get("/",(req,res)=>{
+// ✅ Routes
+app.get("/", (req, res) => {
     res.send("Server is Working");
 });
 
-app.use("/uploads",express.static("uploads"));
-
-//Importing routes 
+// ✅ Importing routes
 import userRoutes from "./routes/user.js";
 import courseRoutes from "./routes/course.js";
 import AdminRoutes from "./routes/Admin.js";
-//import PostRoutes from "./routes/poste.js";
 
+// ✅ Using routes
+app.use("/api", userRoutes);
+app.use("/api", courseRoutes);
+app.use("/api", AdminRoutes);
 
-//Using Routes.
-app.use("/api",userRoutes);
-app.use("/api",courseRoutes);
-app.use("/api",AdminRoutes);
-//app.use("/api", PostRoutes);
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+// ✅ Start Server
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(` Server is running at http://localhost:${port}`);
     connectDb();
 });
